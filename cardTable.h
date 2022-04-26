0045efa8fca8eaa1e067674dd97d5795ba1cfbbe
Jonathan Carlson <JonathanCarlson3712@Hotmail.com>
2022-04-25 22:07:48 -0500

    Added card backs. Set initial card placements on table.

--- cardTable.h
+++ cardTable.h
@@ -43,20 +43,37 @@ public:
     //----------------------------------------------------------------------------------------------
 
     void setCardPositions() {
-        for(short i = 0, j = -100, k = -50; i < cardDeck.deck.size(); i++, j -= 40, k -= 10) {
-            cardDeck.deck[i].cardSprite.setOrigin(j, k);
-            if(i == 25) {
-                j = -60;
-            }
-	    }
+
+        // for(short i = 0, j = -100, k = -50; i < cardDeck.deck.size(); i++, j -= 40, k -= 10) {
+        //     cardDeck.deck[i].cardSprite.setOrigin(j, k);
+        //     if(i == 25) {
+        //         j = -60;
+        //     }
+	    // }
+
+        cardDeck.deck[0].cardSprite.setOrigin(card_1_Origin.first, card_1_Origin.second);
+        cardDeck.deck[1].cardSprite.setOrigin(card_2_Origin.first, card_2_Origin.second);
+        cardDeck.deck[2].cardSprite.setOrigin(card_3_Origin.first, card_3_Origin.second);
+        cardDeck.deck[3].cardSprite.setOrigin(card_4_Origin.first, card_4_Origin.second);        
+
+        cardDeck.cardBacks[0].setOrigin(cardBacks_1_Origin.first, cardBacks_1_Origin.second);
+        cardDeck.cardBacks[1].setOrigin(cardBacks_2_Origin.first, cardBacks_2_Origin.second);
+        cardDeck.cardBacks[2].setOrigin(cardBacks_3_Origin.first, cardBacks_3_Origin.second);
+        cardDeck.cardBacks[3].setOrigin(cardBacks_4_Origin.first, cardBacks_4_Origin.second);              
     }
 
     //----------------------------------------------------------------------------------------------
 
     void drawCardsOnTable() {
-        for(short i = 0; i < cardDeck.deck.size(); i++) {
-			globalData->window.draw(cardDeck.deck[i].cardSprite);
-		}
+        // for(short i = 0; i < cardDeck.deck.size(); i++) {
+		// 	globalData->window.draw(cardDeck.deck[i].cardSprite);
+		// }
+
+        for(short i = 0; i < 4; i++) {
+            globalData->window.draw(cardDeck.deck[i].cardSprite);
+            globalData->window.draw(cardDeck.cardBacks[i]);
+
+        }
     }
 
 private:
@@ -65,6 +82,18 @@ private:
     CardDeck     cardDeck; 
     string       fontFile = "Fonts/Robusta-Regular.ttf";
 	sf::Font     font; 
+
+    pair<short, short> card_1_Origin = {-491, -375};
+    pair<short, short> card_2_Origin = {-741, -230};
+    pair<short, short> card_3_Origin = {-566, -158};
+    pair<short, short> card_4_Origin = {-666, -447};
+
+    pair<short, short> cardBacks_1_Origin = {-491, -545};
+    pair<short, short> cardBacks_2_Origin = {-741, -60};
+    pair<short, short> cardBacks_3_Origin = {-441, -158};
+    pair<short, short> cardBacks_4_Origin = {-791, -447};    
+
+    // pair<short, short> cardBackOrigin = {-435, -302};
 };
 
 #endif // CARDTABLE_H
